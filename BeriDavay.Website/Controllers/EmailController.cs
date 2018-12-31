06d2aca5bf313bc5a1d4de15581f23e6f92b1947
Dmitry <prybylnov@qarea.com>
2018-12-31 15:46:35 +0200

    Emails separate

--- BeriDavay.Website/Controllers/EmailController.cs
+++ BeriDavay.Website/Controllers/EmailController.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using BeriDavay.Website.Controllers.Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Web.Http;
 using BeriDavay.Website.Models;
 using Microsoft.Extensions.Options;
 using SendGrid;
@@ -20,13 +17,6 @@ namespace BeriDavay.Website.Controllers
     public class EmailController : ControllerBase
     {
 
-        private readonly SendGridSettings _sendGridConfig;
-
-        public EmailController(IOptions<SendGridSettings> sendGridSettings)
-        {
-            _sendGridConfig = sendGridSettings.Value;
-        }
-
         /// <summary>
         /// Send mesage 
         /// </summary>
@@ -35,11 +25,14 @@ namespace BeriDavay.Website.Controllers
         /// <param name="takeGive"> true if "Give"</param>
         /// <returns></returns>
         [HttpGet]
+        [ActionName("SendPermitEmail")]
         public async Task<IActionResult> SendPermitEmail(string email, string requesterName, bool takeGive = true)
         {
             string subject = takeGive ? "Some one gave you permit!" : "Some one ask you for permit!";
 
-            var result = await SendEmail(email, subject);
+            string emailTemplate = "Bla bla bla"; //Todo: add template
+
+            var result = await EmailService.SendEmail(email, subject, emailTemplate);
 
             if (!result.ResultStatus)
             {
@@ -53,65 +46,6 @@ namespace BeriDavay.Website.Controllers
 
 
 
-        private async Task<BaseResult> SendEmail(string email, string subject)
-        {
-            var result = new BaseResult();
-
-            MailAddress addr;
-            try
-            {
-                addr = new MailAddress(email);
-            }
-            catch (Exception e)
-            {
-                result.ResultStatus = false;
-
-                var error = new BaseError()
-                {
-                    StatusCode = null,
-                    ErrorDescription = "Faild Email address!",
-                    ExceptionMessage = e.Message
-                };
-
-                result.Errors.Add(error);
-
-                return result;
-            }
-
-            var status = await SendEmailCore(email, "Test", "Test!!!");
-
-            if (status != HttpStatusCode.Accepted)
-            {
-                result.ResultStatus = false;
-
-                var error = new BaseError()
-                {
-                    StatusCode = status,
-                    ErrorDescription = "Check SendEmailCore, Send fails",
-                    ExceptionMessage = null
-                };
-                result.Errors.Add(error);
-
-                return result;
-            }
-
-            return new BaseResult(){ResultStatus = true};
-        }
-
-
-
-
-        private async Task<HttpStatusCode> SendEmailCore(string email, string subject, string htmlContent)
-        {
-            var apiKey = _sendGridConfig.SendGridApiKey;
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("permit@beridabay.org", subject);
-            var to = new EmailAddress(email);
-            var plainTextContent = Regex.Replace(htmlContent, "<[^>]*>", "");
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
-
-            return response.StatusCode;
-        }
+        
     }
 }
---
+++ BeriDavay.Website/Controllers/Services/EmailService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using BeriDavay.Website.Models;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace BeriDavay.Website.Controllers.Services
+{
+    public class EmailService
+    {
+
+        private readonly SendGridSettings _sendGridConfig;
+
+        public EmailService(IOptions<SendGridSettings> sendGridSettings)
+        {
+            _sendGridConfig = sendGridSettings.Value;
+        }
+
+        public static async Task<BaseResult> SendEmail(string email, string subject, string emailTemplate)
+        {
+            var result = new BaseResult();
+            //ToDo: Template logic!!!
+            MailAddress addr;
+            try
+            {
+                addr = new MailAddress(email);
+            }
+            catch (Exception e)
+            {
+                result.ResultStatus = false;
+
+                var error = new BaseError()
+                {
+                    StatusCode = null,
+                    ErrorDescription = "Faild Email address!",
+                    ExceptionMessage = e.Message
+                };
+
+                result.Errors.Add(error);
+
+                return result;
+            }
+
+            var status = await SendEmailCore(email, subject, emailTemplate);
+
+            if (status != HttpStatusCode.Accepted)
+            {
+                result.ResultStatus = false;
+
+                var error = new BaseError
+                {
+                    StatusCode = status,
+                    ErrorDescription = "Check SendEmailCore, Send fails",
+                    ExceptionMessage = null
+                };
+                result.Errors.Add(error);
+
+                return result;
+            }
+
+            return new BaseResult { ResultStatus = true };
+        }
+
+
+
+
+        private async Task<HttpStatusCode> SendEmailCore(string email, string subject, string htmlContent)
+        {
+            var apiKey = _sendGridConfig.SendGridApiKey;
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("permit@beridabay.org", subject);
+            var to = new EmailAddress(email);
+            var plainTextContent = Regex.Replace(htmlContent, "<[^>]*>", "");
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+
+            return response.StatusCode;
+        }
+
+    }
+}
